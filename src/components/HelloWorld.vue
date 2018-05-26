96e639ca4d637585d7d0d5c6c199ce0df74d760a
‘fenghua’ <597709174@qq.com>
2018-05-26 11:37:01 +0800

    init vuepro

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+   <h1>{{msg}}</h1>
+   <ul>
+     <li v-for="(item, index) in list" :key="index">{{item}}</li>
+   </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'vue + mock'
+    }
+  },
+  props: {
+    list: {
+      type: Array
+    }
+  },
+  mounted () {
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <HelloWorld :list="list"/>
+</template>
+
+<script>
+import axios from 'axios'
+import HelloWorld from '@/components/HelloWorld'
+export default {
+  name: 'index',
+  data () {
+    return {
+      list: []
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  mounted () {
+    axios.post('/api/list').then((res) => {
+      console.log(res.data, 'res====')
+      this.list = res.data.list
+    })
+    axios.get('/api/user').then((res) => {
+      console.log(res, 'res')
+    })
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 30px 10px;
+}
+
+</style>
